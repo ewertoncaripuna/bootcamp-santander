915ab134c8284364c975eb53235540f31246790f
ewertoncaripuna <ewertoncaripuna@outlook.com>
2024-05-08 09:49:30 -0300

    add: project about execution control and exceptions

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -6,22 +6,29 @@ public class ContaTerminal {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("*********Seja bem vindo!*********");
+//        System.out.println("Cadastre sua conta.");
+//
+//        System.out.print("Insira o número da conta: ");
+//        int numero = sc.nextInt();
+//        sc.nextLine();
+//        System.out.print("Insira a agência: ");
+//        String agencia = sc.nextLine();
+//        System.out.print("Insira o nome do cliente: ");
+//        String nome = sc.nextLine();
+//        System.out.print("Insira o saldo da conta: ");
+//        double saldo = sc.nextDouble();
+//
+//        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
+//                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+//
 
-        System.out.println("*********Seja bem vindo!*********");
-        System.out.println("Cadastre sua conta.");
+            for(int x=1;x<=10;x++){
+                if(x % 2==0) //% operador que representa módulo de uma divisão
+                    System.out.println(x);
+            }
 
-        System.out.print("Insira o número da conta: ");
-        int numero = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Insira a agência: ");
-        String agencia = sc.nextLine();
-        System.out.print("Insira o nome do cliente: ");
-        String nome = sc.nextLine();
-        System.out.print("Insira o saldo da conta: ");
-        double saldo = sc.nextDouble();
-
-        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
         sc.close();
     }
